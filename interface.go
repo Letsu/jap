39a4d1b2518343e5b11dd3d464b78d66df5af4cd
johannesbindriem <johannes.bindriem@yahoo.de>
2022-10-13 22:40:43 +0200

    update how to find some stuff on interfaces

--- interface.go
+++ interface.go
@@ -1,6 +1,7 @@
 package jap
 
 import (
+	"reflect"
 	"regexp"
 	"strconv"
 	"strings"
@@ -9,17 +10,17 @@ import (
 type CiscoInterface struct {
 	Identifier         string
 	SubInterface       int
-	Description        string
-	Shutdown           bool
-	Trunk              bool
-	AccessPort         bool
-	NativeVlan         int
+	Description        string `cmd:"description ([[:print:]]+)"`
+	Shutdown           bool   `cmd:"shutdown"`
+	Trunk              bool   `cmd:"switchport mode trunk"`
+	AccessPort         bool   `cmd:"switchport mode access"`
+	NativeVlan         int    `cmd:"switchport trunk native vlan ([0-9]+)"`
 	Ips                []Ip
-	Vrf                string
-	DhcpSnoopingThrust bool
-	AccessVlan         int
-	VoiceVlan          int
-	IPHHelperAddresses []string
+	Vrf                string `cmd:"ip vrf forwarding ([[:print:]]+)"`
+	DhcpSnoopingThrust bool   `cmd:"ip dhcp snooping trust"`
+	AccessVlan         int    `cmd:"switchport access vlan ([0-9]+)"`
+	VoiceVlan          int    `cmd:"switchport voice vlan ([0-9]+)"`
+	IPHelperAddresses  []string
 }
 
 type Ip struct {
@@ -38,20 +39,6 @@ func ParseInterface(part string) (CiscoInterface, error) {
 	inter.Identifier = identifier[0]
 	if len(identifier) > 1 {
 		inter.SubInterface, _ = strconv.Atoi(identifier[1])
-	}
-
-	// Get description
-	re = regexp.MustCompile(`description ([[:print:]]+)`)
-	descriptionPart := re.FindStringSubmatch(part)
-	if len(descriptionPart) > 0 {
-		inter.Description = descriptionPart[1]
-	}
-
-	// Get port shutdown
-	re = regexp.MustCompile(`shutdown`)
-	inter.Shutdown = false
-	if re.MatchString(part) {
-		inter.Shutdown = true
 	}
 
 	// Get ipv4 addresses
@@ -75,61 +62,35 @@ func ParseInterface(part string) (CiscoInterface, error) {
 		inter.Ips = append(inter.Ips, ipAdd)
 	}
 
-	// VRF Forwarding
-	re = regexp.MustCompile(`ip vrf forwarding ([[:print:]]+)`)
-	vrf := re.FindStringSubmatch(part)
-	if len(vrf) > 0 {
-		inter.Vrf = vrf[1]
-	}
-
 	//IP Helper Addresses
 	re = regexp.MustCompile(`ip helper-address (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})`)
 	ips = re.FindAllStringSubmatch(part, -1)
 	for _, intIp := range ips {
-		inter.IPHHelperAddresses = append(inter.IPHHelperAddresses, intIp[1])
+		inter.IPHelperAddresses = append(inter.IPHelperAddresses, intIp[1])
 	}
 
 	//
-	// Get Trunk stuff
+	// Get all the rest stuff
 	//
-	if strings.Contains(part, "switchport mode trunk") && !strings.Contains(part, "switchport mode access") {
-		inter.Trunk = true
-	}
-
-	//Native Vlan
-	re = regexp.MustCompile(`switchport trunk native vlan ([0-9]+)`)
-	nativeVlan := re.FindStringSubmatch(part)
-	if len(nativeVlan) > 0 {
-		inter.NativeVlan, _ = strconv.Atoi(nativeVlan[1])
-	}
-
-	// IP dhcp snopping thrust
-	inter.DhcpSnoopingThrust = false
-	if strings.Contains(part, "ip dhcp snooping trust") {
-		inter.DhcpSnoopingThrust = true
-	}
-
-	//
-	// Get Access Stuff
-	//
-
-	// Access state
-	if strings.Contains(part, "switchport mode access") && !strings.Contains(part, "switchport mode trunk") {
-		inter.AccessPort = true
-	}
-
-	//Access vlan
-	re = regexp.MustCompile(`switchport access vlan ([0-9]+)`)
-	accessVlan := re.FindStringSubmatch(part)
-	if len(accessVlan) > 0 {
-		inter.AccessVlan, _ = strconv.Atoi(accessVlan[1])
-	}
-
-	// Voice Vlan
-	re = regexp.MustCompile(`switchport voice vlan ([0-9]+)`)
-	voiceVlan := re.FindStringSubmatch(part)
-	if len(voiceVlan) > 0 {
-		inter.VoiceVlan, _ = strconv.Atoi(voiceVlan[1])
+	t := reflect.TypeOf(inter)
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		tag := field.Tag.Get("cmd")
+		if tag != "" {
+			re = regexp.MustCompile(tag)
+			data := re.FindStringSubmatch(part)
+			if len(data) > 1 && field.Type.Kind() == reflect.String {
+				reflect.ValueOf(&inter).Elem().Field(i).SetString(data[1])
+			} else if len(data) > 1 && field.Type.Kind() == reflect.Int {
+				value, err := strconv.ParseInt(data[1], 10, 64)
+				if err != nil {
+					return CiscoInterface{}, err
+				}
+				reflect.ValueOf(&inter).Elem().Field(i).SetInt(value)
+			} else if len(data) == 1 && field.Type.Kind() == reflect.Bool {
+				reflect.ValueOf(&inter).Elem().Field(i).SetBool(true)
+			}
+		}
 	}
 
 	return inter, nil
