9e4be35515e3c0a4ab079008b13662c97e83a5b2
johannesbindriem <johannes.bindriem@yahoo.de>
2022-10-17 17:25:25 +0200

    add float type and clean up

--- interface.go
+++ interface.go
@@ -13,23 +13,25 @@ type CiscoInterface struct {
 	Identifier            string
 	SubInterface          int
 	Ips                   []Ip
-	IPHelperAddresses     []string
-	Shutdown              bool   `reg:"shutdown" cmd:"shutdown"`
-	Trunk                 bool   `reg:"switchport mode trunk" cmd:"switchport mode trunk"`
-	AccessPort            bool   `reg:"switchport mode access" cmd:"switchport mode access"`
-	DhcpSnoopingThrust    bool   `reg:"ip dhcp snooping trust" cmd:"ip dhcp snooping trust"`
-	PortSecurity          bool   `reg:"switchport port-security" cmd:"switchport port-security"`
-	Description           string `reg:"description ([[:print:]]+)" cmd:"description %s"`
-	Vrf                   string `reg:"ip vrf forwarding ([[:print:]]+)" cmd:"ip vrf forwarding %s"`
-	PortSecurityAgingType string `reg:"switchport port-security type (absolute|inactivity)" cmd:"switchport port-security type  %s"`
-	PortSecurityViolation string `reg:"switchport port-security violation (protect|restrict|shutdown)" cmd:"switchport port-security violation %s"`
-	STPportfast           string `reg:"spanning-tree portfast (disable|edge|network)" cmd:"spanning-tree portfast %s"`
-	STPbpduguard          string `reg:"spanning-tree bpduguard (disable|enable)" cmd:"spanning-tree bpduguard %s"`
-	NativeVlan            int    `reg:"switchport trunk native vlan ([0-9]+)" cmd:"switchport trunk native vlan %d"`
-	AccessVlan            int    `reg:"switchport access vlan ([0-9]+)" cmd:"switchport access vlan %d"`
-	VoiceVlan             int    `reg:"switchport voice vlan ([0-9]+)" cmd:"switchport voice vlan %d"`
-	PortSecurityMaximum   int    `reg:"switchport port-security maximum ([0-9]+)" cmd:"switchport port-security maximum %d"`
-	PortSecurityAgingTime int    `reg:"switchport port-security aging time ([0-9]+)" cmd:"switchport port-security aging time %d"`
+	IPHelperAddresses     []string `reg:"ip helper-address (\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})" cmd:"ip helper-address %s"`
+	TrunkAllowedVlan      []int    `reg:"ip helper-address (\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})" cmd:"ip helper-address %s"`
+	Shutdown              bool     `reg:"shutdown" cmd:"shutdown"`
+	Trunk                 bool     `reg:"switchport mode trunk" cmd:"switchport mode trunk"`
+	AccessPort            bool     `reg:"switchport mode access" cmd:"switchport mode access"`
+	DhcpSnoopingThrust    bool     `reg:"ip dhcp snooping trust" cmd:"ip dhcp snooping trust"`
+	PortSecurity          bool     `reg:"switchport port-security" cmd:"switchport port-security"`
+	Description           string   `reg:"description ([[:print:]]+)" cmd:"description %s"`
+	Vrf                   string   `reg:"ip vrf forwarding ([[:print:]]+)" cmd:"ip vrf forwarding %s"`
+	PortSecurityAgingType string   `reg:"switchport port-security type (absolute|inactivity)" cmd:"switchport port-security type  %s"`
+	PortSecurityViolation string   `reg:"switchport port-security violation (protect|restrict|shutdown)" cmd:"switchport port-security violation %s"`
+	STPportfast           string   `reg:"spanning-tree portfast (disable|edge|network)" cmd:"spanning-tree portfast %s"`
+	STPbpduguard          string   `reg:"spanning-tree bpduguard (disable|enable)" cmd:"spanning-tree bpduguard %s"`
+	NativeVlan            int      `reg:"switchport trunk native vlan ([0-9]+)" cmd:"switchport trunk native vlan %d"`
+	AccessVlan            int      `reg:"switchport access vlan ([0-9]+)" cmd:"switchport access vlan %d"`
+	VoiceVlan             int      `reg:"switchport voice vlan ([0-9]+)" cmd:"switchport voice vlan %d"`
+	PortSecurityMaximum   int      `reg:"switchport port-security maximum ([0-9]+)" cmd:"switchport port-security maximum %d"`
+	PortSecurityAgingTime int      `reg:"switchport port-security aging time ([0-9]+)" cmd:"switchport port-security aging time %d"`
+	SCBroadcastLevel      float64  `reg:"storm-control broadcast level ([0-9.]+)" cmd:"storm-control broadcast level %f"`
 }
 
 type Ip struct {
@@ -88,16 +90,30 @@ func ParseInterface(part string) (CiscoInterface, error) {
 			re = regexp.MustCompile(tag)
 			// @todo check if no is with the command!
 			data := re.FindStringSubmatch(part)
-			if len(data) > 1 && field.Type.Kind() == reflect.String {
+			if len(data) == 0 {
+				continue
+			}
+
+			switch field.Type.Kind() {
+			case reflect.String:
 				reflect.ValueOf(&inter).Elem().Field(i).SetString(data[1])
-			} else if len(data) > 1 && field.Type.Kind() == reflect.Int {
+				break
+			case reflect.Int:
 				value, err := strconv.ParseInt(data[1], 10, 64)
 				if err != nil {
 					return CiscoInterface{}, err
 				}
 				reflect.ValueOf(&inter).Elem().Field(i).SetInt(value)
-			} else if len(data) == 1 && field.Type.Kind() == reflect.Bool {
+				break
+			case reflect.Bool:
 				reflect.ValueOf(&inter).Elem().Field(i).SetBool(true)
+				break
+			case reflect.Float64:
+				float, err := strconv.ParseFloat(data[1], 64)
+				if err != nil {
+					return CiscoInterface{}, err
+				}
+				reflect.ValueOf(&inter).Elem().Field(i).SetFloat(float)
 			}
 		}
 	}
