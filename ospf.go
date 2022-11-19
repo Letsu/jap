9bbcde4a279d39a5e013267e55a2806ba1bba635
johannesbindriem <johannes.bindriem@yahoo.de>
2022-11-19 11:37:08 +0100

    basic ospf parsing

--- ospf.go
+++ ospf.go
@@ -4,16 +4,30 @@ type Ospf struct {
 	LogAdjacencyChange      bool `reg:"log-adjacency-changes detail" cmd:"log-adjacency-changes detail"`
 	PassiveInterfaceDefault bool `reg:"passive-interface default" cmd:"passive-interface default"`
 	PassiveInterface        []int
-	Network                 []OspfNetwork
+	Network                 []OspfNetwork `reg:"network.*"`
 }
 
 type OspfNetwork struct {
-	NetworkNumber string `reg:"network (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) (?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) area (?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\d+)"`
-	WildCard      string `reg:"network (?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) area (?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\d+)"`
-	Area          string `reg:"network (?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) (?:\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) area (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\d+)"`
+	NetworkNumber string `reg:"network (\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}) (?:\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}) area (?:\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}|\\d+)"`
+	WildCard      string `reg:"network (?:\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}) (\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}) area (?:\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}|\\d+)""`
+	Area          string `reg:"network (?:\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}) (?:\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}) area (\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}|\\d+)""`
 }
 
 func ParseOSPF(part string) (Ospf, error) {
-
+	var ospf Ospf
+	err := ProcessParse(part, &ospf)
+	if err != nil {
+		return Ospf{}, err
+	}
 	return Ospf{}, nil
+}
+
+func (o Ospf) Parse(part string) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (o Ospf) Generate() (string, error) {
+	//TODO implement me
+	panic("implement me")
 }
